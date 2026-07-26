ddf998f853ef82e3426ac5c5f18ccbc33b28e581
forgetechnolgies <forge.game.devs@gmail.com>
2026-07-26 02:23:54 +0200

    Publish managed Wersee Sites dist

--- dist/assets/front-channel-logout.js
+++ dist/assets/front-channel-logout.js
@@ -1,1 +1,1 @@
-import{s as a,f as t}from"./authSessionCleanup-NPIFfEI0.js";import"./vendor-supabase-_AuQRo9U.js";(async()=>{try{await a.auth.signOut({scope:"local"})}catch{}finally{t()}})();
+import{s as a,e as t}from"./authSessionCleanup-D10ueJO8.js";import"./vendor-supabase-_AuQRo9U.js";(async()=>{try{await a.auth.signOut({scope:"local"})}catch{}finally{t()}})();
